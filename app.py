987710ed5ff2d5c2a331244d948cea4c354d3903
1508900 <sdruiz@afi.es>
2026-05-04 10:38:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ MARKET_TICKERS = [
     "^GSPC", "^IXIC", "^STOXX50E", "ACWI", "EEM", "ILF", "MCHI", "EWY",
     "EURUSD=X", "DX-Y.NYB", "EURJPY=X", "EURGBP=X", "USDJPY=X",
     "TTF=F", "BZ=F", "CL=F", "GC=F", "SI=F", "HG=F", "ALI=F", "NI=F", "ZNC=F",
-    "^VIX", "V2TX.DE"
+    "^VIX", "OVS.EX"
 ]
 
 HOLDING_TICKERS = [
